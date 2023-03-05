89a92de24d7dca799aa541378ff5a9656ea435cc
collins mbathi <perezcoolins@gmail.com>
2023-03-05 16:00:01 +0300

    fixed it all

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -7,12 +7,12 @@ module.exports = async function(req, res, next) {
             
         const file = req.files.file;
 
-        if(file.size > 1024 * 1024){
+        if(file?.size > 1024 * 1024){
             removeTmp(file.tempFilePath)
             return res.status(400).json({msg: "Size too large."})
         } // 1mb
 
-        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+        if(file?.mimetype !== 'image/jpeg' && file?.mimetype !== 'image/png'){
             removeTmp(file.tempFilePath)
             return res.status(400).json({msg: "File format is incorrect."})
         }
